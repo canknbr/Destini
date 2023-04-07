7995e64006d61389b7229ea2406b962b1c2d669f
canknbr <cankanbur1@gmail.com>
2023-04-07 16:44:52 +0300

    added sample

---
+++ Destini/Story.swift
@@ -0,0 +1,16 @@
+//
+//  Story.swift
+//  Destini
+//
+//  Created by Can Kanbur on 7.04.2023.
+//
+
+import Foundation
+
+struct Story {
+    let title: String
+     let choice1: String
+     let choice1Destination: Int
+     let choice2: String
+     let choice2Destination: Int
+}
--- Destini/ViewController.swift
+++ Destini/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var choiceTwo: UIButton!
+    @IBOutlet weak var choiceOne: UIButton!
+    @IBOutlet weak var storyLabel: UILabel!
+    
+    var storyBrain = StoryBrain()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+updateUI()
     }
 
-
+    @IBAction func choiceClicked(_ sender: UIButton) {
+        var userChoice = sender.currentTitle!
+        storyBrain.getNextStory(userChoice)
+        updateUI()
+    }
+    
+    
+    func updateUI(){
+        storyLabel.text = storyBrain.getStoryTitle()
+        choiceOne.setTitle(storyBrain.getChoiceOne(), for: UIControl.State.normal)
+        choiceTwo.setTitle(storyBrain.getChoiceTwo(), for: UIControl.State.normal)
+    }
+    
 }
 
